bdfb7bd7ae42636b833bb233bf6e4a47423032b7
Talha Ahmed <talhaahmedashraf@gmail.com>
2021-11-04 10:59:21 +0500

    add tb for pc

---
+++ Testbench/Pc_tb.py
@@ -0,0 +1,24 @@
+@block
+def testing():
+    pc_input, pc, pc4 = [Signal(intbv(0,0,2**DW)) for i in range(3)]
+    clk = Signal(bool(0))
+    pCounter = PC(clk, pc_input, pc, pc4)
+
+    @instance
+    def run():
+        for i in range(0,10):
+            pc_input.next = i
+            yield clk.posedge
+            print(f"{pc} {pc4}")
+            # yield clk
+        raise StopSimulation
+
+    @always(delay(10))
+    def clkgen():
+        clk.next = not clk
+    
+    return pCounter, run, clkgen
+
+tb = testing()
+tb.config_sim(trace=True)
+tb.run_sim()
