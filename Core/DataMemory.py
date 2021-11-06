1524b3ed0c56888c8fe2b59292db88b36e50e78c
Talha Ahmed <talhaahmedashraf@gmail.com>
2021-11-06 13:02:13 +0500

    update dmem

--- Core/DataMemory.py
+++ Core/DataMemory.py
@@ -8,23 +8,22 @@ def DataMemory(clk, addr, data_in, data_out, we, rd):
 
     mem = [Signal(intbv(0, -DW, DW)) for i in range(ROWS)]
 
-    @always(clk.posedge)
+    @always_comb
     def read():
-        # if rd:
-        data_out.next = mem[int(addr)]  & (rd << 32)
+        data_out.next = mem[int(addr)] if rd else 0
 
     @always(clk.posedge)
     def write():
-        # if we:
-        mem[addr].next = data_in & (we << 32)
+        if we:
+            mem[addr].next = data_in
 
     return read, write
 
 def toGenerateVerilog():
     clk = Signal(bool(0))
     addr = Signal(intbv(0)[10:])
-    data_in = Signal(intbv(0, -DW, DW)[32:])
-    data_out = Signal(intbv(0, -DW, DW)[32:])
+    data_in = Signal(intbv(0, -DW, DW))
+    data_out = Signal(intbv(0, -DW, DW))
     we = Signal(bool(0))
     rd = Signal(bool(0))
     # mem_data = Signal(intbv(0)[32:])
